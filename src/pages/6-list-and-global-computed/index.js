28450fa7725979d7badb34a5abfc2e0ebf7e6e50
秦真 <739694218@qq.com>
2017-03-06 13:40:49 +0800

    增强 listview 购物车功能，加入选中项，删除项功能

--- src/pages/6-list-and-global-computed/index.js
+++ src/pages/6-list-and-global-computed/index.js
@@ -11,6 +11,7 @@ import {
   StyleSheet,
   ListView,
   Text,
+  TouchableOpacity
 } from 'react-native';
 
 import {
@@ -42,16 +43,25 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#F1F1F1'
   },
+  name: {
+    fontSize: 18,
+    color: '#333'
+  },
   price: {
+    color: '#F3F',
     marginLeft: 10,
     flex: 1,
   },
+  count: {
+    marginRight: 10
+  },
   btn: {
-    padding: 8,
+    padding: 5,
+    marginHorizontal: 2,
     borderWidth: 1,
   },
   active: {
-    backgroundColor: '#F7F7F7'
+    backgroundColor: '#e1fEfE'
   }
 });
 
@@ -59,35 +69,58 @@ class CartItem {
 
   name = '';
   price = 0;
+  cart = null;
 
   @observable
   count = 0;
 
-  // @observable
-  // selctedItem = null;
-
-  constructor(name, price) {
+  constructor(name, price, cart) {
     this.name = name;
     this.price = price;
+    this.cart = cart;
   }
 
-  @action
+  @computed
+  get selected() {
+    return this.cart.selectedItem === this;
+  }
+
+  @action('商品数量增加')
   inc = () => {
     ++this.count;
   }
 
-  @action
+  @action('商品数量减少')
   dec = () => {
     if (this.count > 0) {
       --this.count;
     }
   }
+
+  @action('当前项被选中')
+  select = () => {
+    this.cart.selectedItem = this;
+  }
+
+  @action('删除当前项')
+  del = () => {
+    this.cart.items.remove(this);
+    // 如果删除的项刚好是选中的那项
+    if (this.selected) {
+      this.cart.selectedItem = null;
+    }
+  }
+
 };
 
 class Cart {
 
   @observable
   items = [];
+
+  // 选中的项
+  @observable
+  selectedItem = null;
 
   constructor() {
     this.initCart();
@@ -99,11 +132,17 @@ class Cart {
       this.items.push(new CartItem(
         `商品${i}`,
         Math.floor(Math.random() * 100000) / 100,
+        this
       ));
     }
-    this.items.push(new CartItem('附加商品1', 100));
-    this.items.push(new CartItem('附加商品2', 123.4));
-    this.items.push(new CartItem('附加商品3', 12345));
+    this.items.push(new CartItem('附加商品1', 100, this));
+    this.items.push(new CartItem('附加商品2', 123.4, this));
+    this.items.push(new CartItem('附加商品3', 12345, this));
+  }
+
+  @computed
+  get itemList() {
+    return this.items;
   }
 
   @computed
@@ -119,22 +158,24 @@ class Cart {
 
 @observer
 class Item extends Component {
+  
   static propTypes = {
     data: PropTypes.instanceOf(CartItem),
   };
+
   render() {
     const {
       data
     } = this.props;
-    console.log('Item render ', data);
     return (
-      <View style={styles.item}>
-        <Text>{data.name}</Text>
+      <TouchableOpacity style={[styles.item, data.selected && styles.active ]} onPress={data.select}>
+        <Text style={styles.name}>{data.name}</Text>
         <Text style={styles.price}>${data.price}</Text>
-        <Text>{data.count}</Text>
+        <Text style={styles.count}>{data.count}</Text>
         <Text style={styles.btn} onPress={data.inc}>+</Text>
         <Text style={styles.btn} onPress={data.dec}>-</Text>
-      </View>
+        <Text style={styles.btn} onPress={data.del}>x</Text>
+      </TouchableOpacity>
     );
   }
 };
@@ -142,15 +183,18 @@ class Item extends Component {
 const Info = observer(function({
   cart
 }) {
-  console.log('Info render ', cart);
+  const { count, price, selectedItem } = cart;
   return (
     <Text>
-      Count: {`${cart.count}`} {'\n'}
-      Price: {cart.price.toFixed(2)} {'\n'}
+      Count: {count} {'\n'}
+      Price: {price.toFixed(2)} {'\n'}
+      {selectedItem && <Text>当前选中商品：{selectedItem.name}</Text>}
     </Text>
   );
 });
 
+
+@observer // 此处加入 observer 为了删除功能
 export default class Demo extends Component {
   static title = '6 - List & Global Computed';
 
@@ -167,11 +211,10 @@ export default class Demo extends Component {
   };
 
   render() {
-    console.log('Demo render ', this.cart);
     return (
       <View style={styles.container}>
         <ListView
-          dataSource={this.ds.cloneWithRows(this.cart.items.slice(0))}
+          dataSource={this.ds.cloneWithRows(this.cart.itemList.slice())}
           renderRow={this.renderRow}
           style={styles.cartList}
         />
